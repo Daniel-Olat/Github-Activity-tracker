c8f7690a1c58a35fb748e438e1753e5009fa2642
Daniel Olatunji <danielolatunji2907@gmail.com>
2025-10-21 12:52:18 +0100

    Deployment

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from main import fetch_github_data
 
 app = Flask(__name__)
-# keep a simple list of recent activities (sessionless global list)
+
 activities = []
 
 
@@ -14,7 +14,6 @@ def home():
     input_text = request.form.get('username')
     if input_text:
       result = fetch_github_data(input_text)
-      # result is a list of activity strings; replace activities with latest
       activities = result or []
 
   return render_template('index.html', activities=activities)

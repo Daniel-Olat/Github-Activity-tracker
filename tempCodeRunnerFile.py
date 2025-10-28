0053d44b270aa3a0377c546d0e6056b91bbb2869
Daniel Olatunji <danielolatunji2907@gmail.com>
2025-10-28 22:32:43 +0100

    Add temp runner file and update requirements

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from main import fetch_github_data
+
+app = Flask(__name__)
+
+activities = []
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+  global activities
+  result = None
+  if request.method == 'POST':
+    input_text = request.form.get('username')
+    if input_text:
+      result = fetch_github_data(input_text)
+      activities = result or []
+
+  return render_template('index.html', activities=activities)
+
+
